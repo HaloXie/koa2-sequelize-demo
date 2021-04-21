4f9fd6012534f93f4d83a7879e3c296bcb4be845
Justin Xie <minghao.xie@foxmail.com>
2021-04-22 00:25:46 +0800

    fix the typs not found

--- src/include.d/global.d.ts
+++ src/include.d/global.d.ts
@@ -1,8 +1,7 @@
 type Optional<T> = T | undefined;
-type Pickable = Record<string, any>;
+type Pickable = Record<string, unknown>;
 type NonFalsy<T> = Exclude<T, '' | 0 | false | null | undefined | void | never>;
 type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-
 declare namespace NodeJS {
   export interface Global {
     removeOptional: (obj: Pickable) => Pickable;
@@ -12,3 +11,9 @@ declare namespace NodeJS {
     conditional: <T>(exp: T) => Optional<NonFalsy<T>>;
   }
 }
+
+declare const removeOptional: (obj: Pickable) => Pickable;
+declare const has: (data: unknown, property: string) => boolean;
+declare const pick: (obj: Pickable, ...args: string[]) => Pickable;
+declare const notFalsy: <T>(value: T) => boolean;
+declare const conditional: <T>(exp: T) => Optional<NonFalsy<T>>;
